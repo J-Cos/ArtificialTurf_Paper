c55ae6333e6840c7bef4232d611dd377ef7a5c01
J <j.williams20@imperial.ac.uk>
2023-04-25 13:47:32 +0100

    Update GetClassCoverageDf().r
    Simplified function

--- Functions/GetClassCoverageDf().r
+++ Functions/GetClassCoverageDf().r
@@ -1,6 +1,7 @@
 GetClassCoverageDf<-function(sc, resPl){
-    sc_freq<- freq(sc, useNA="no")
-    area_m2 <- sc_freq[,"count"]*prod(resPl)
-    landcoverdf <- data.frame(landcover=classes, area_m2=area_m2)
+    landcoverdf<- freq(sc, useNA="no") %>%
+        as_tibble %>%
+        mutate(area_m2=count*prod(resPl))
+
     return(landcoverdf)
 }
